e0474daf3e0e977e1f67c64992cf93eb030d1a6f
Ilche Spaseski <ile.spasesky@gmail.com>
2020-03-22 14:43:48 +0100

    tax type variable added

--- Item.java
+++ Item.java
@@ -4,12 +4,16 @@ class Item {
 	double price;
 
 	//TODO add variable.
-
+      int a=15;
+	  int b=5;
+	  int v=0;
 	//TODO constructor
-
+           
+			
 	//TODO setters and getters
-
+             
+  
 	double taxReturn () {
-		//TODO
+		
 	}
 }
