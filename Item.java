0c1b25471ee73b0d5b1cf67529ecee6ab296a09d
Ilche Spaseski <ile.spasesky@gmail.com>
2020-03-22 14:47:24 +0100

    taxReturn method finished

--- Item.java
+++ Item.java
@@ -9,11 +9,37 @@ class Item {
 	  int v=0;
 	//TODO constructor
            
-			
+			 Item (int id, String name, double price) 
+			{
+				this.id = id;
+				strcpy(this.name, name);
+				this.price = price;
+			}
 	//TODO setters and getters
-             
+              void setId(int s) {
+      id = s;
+    }
+    
+    int getId() {
+      return id;
+	}
+	   void setname(String s) {
+       strcpy(name,s);
+    }
+    
+    String getname() {
+      return name;
+	}
+	   void setPrice(int s) {
+     price = s;
+    }
+    
+    double getPrice() {
+      return price;
+	}
   
 	double taxReturn () {
-		
+		int suma =(price * a / 100) + (price * b /100);
+		return suma * a / 100;
 	}
 }
